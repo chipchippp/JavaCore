11e4c819de91a605019835ce8db4a6fa4063759c
chipchippp <114909102+chipchippp@users.noreply.github.com>
2025-02-06 17:17:17 +0700

    LinkedHashMap and HashMap

--- src/JavaCollection/MapInterface/HashMapExample.java
+++ src/JavaCollection/MapInterface/HashMapExample.java
@@ -1,15 +1,34 @@
 package JavaCollection.MapInterface;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class HashMapExample {
     public static void main(String[] args) {
-        HashMap<Integer, String> hashMap = new HashMap<>();
+//        Map<String, Object> hashMap = new HashMap<>();
+        Map<String, Object> hashMap = new LinkedHashMap<>();
 
-        hashMap.put(1, "One");
-        hashMap.put(2, "Two");
-        hashMap.put(1, "Three"); // Thay thế giá trị key = 1
 
-        System.out.println(hashMap); // {1=Three, 2=Two}
+        hashMap.put("timestamp", new Date());
+        hashMap.put("status", 200);
+        hashMap.put("path", "/api/v1/users");
+        hashMap.put("error", "Not Found");
+        hashMap.put("message", "OK");
+        hashMap.put(null, "A");
+        hashMap.put(null, "B");
+        hashMap.put(null, "C");
+
+        hashMap.put("1", null);
+        hashMap.put("2", null);
+        hashMap.put("3", null);
+
+        for (String key : hashMap.keySet()) {
+            System.out.println(key + ": " + hashMap.get(key));
+        }
+        if (hashMap.containsKey("status")){
+            System.out.println("Key null: " + hashMap.get("status"));
+        }
     }
 }
