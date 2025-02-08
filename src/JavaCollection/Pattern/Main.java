c28988058a7d77362d7ade0ff7f6e2e5efc5c25a
chipchippp <114909102+chipchippp@users.noreply.github.com>
2025-02-08 16:49:37 +0700

    java conversion and date

--- src/JavaCollection/Pattern/Main.java
+++ src/JavaCollection/Pattern/Main.java
@@ -1,7 +1,9 @@
 package JavaCollection.Pattern;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,11 +27,91 @@ public class Main {
 //        Matcher matcher = pattern.matcher("a");
 //        System.out.println(matcher.matches());
 
-        String regex = "[amn]";
-        System.out.println(Pattern.matches(regex, "abcd")); // false (not a or m or n)
-        System.out.println(Pattern.matches(regex, "a")); // true (among a or m or n)
-        System.out.println(Pattern.matches(regex, "ammmna")); // false (m and a comes more than once)
+//        String regex = "[amn]";
+//        System.out.println(Pattern.matches(regex, "abcd")); // false (not a or m or n)
+//        System.out.println(Pattern.matches(regex, "a")); // true (among a or m or n)
+//        System.out.println(Pattern.matches(regex, "ammmna")); // false (m and a comes more than once)
 
+//        String i = "2";
+//        String f = "2.2";
+//        String d = "2.3";
+//        String b = "5.5";
+//
+//        int x = Integer.parseInt(i);
+//        System.out.println(x);
+//
+//        float y = Float.parseFloat(f);
+//        System.out.println(y);
+//
+//        double z = Double.parseDouble(d);
+//        System.out.println(z);
+//
+//        BigDecimal bigDecimal = new BigDecimal(b);
+//        System.out.println(bigDecimal);
 
+//        Date currentDate = new Date();
+//        System.out.println("Current Date: " + currentDate);
+//
+//        String dateToStr = DateFormat.getInstance().format(currentDate);
+//        System.out.println("DateFormat.getInstance(): " + dateToStr);
+//
+//        dateToStr = DateFormat.getDateInstance().format(currentDate);
+//        System.out.println("DateFormat.getDateInstance(): " + dateToStr);
+//
+//        dateToStr = DateFormat.getTimeInstance().format(currentDate);
+//        System.out.println("DateFormat.getTimeInstance(): " + dateToStr);
+//
+//        dateToStr = DateFormat.getDateTimeInstance().format(currentDate);
+//        System.out.println("DateFormat.getDateTimeInstance(): " + dateToStr);
+//
+//        dateToStr = DateFormat.getTimeInstance(DateFormat.SHORT).format(currentDate);
+//        System.out.println("DateFormat.getTimeInstance(DateFormat.SHORT): " + dateToStr);
+//
+//        dateToStr = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(currentDate);
+//        System.out.println("DateFormat.getTimeInstance(DateFormat.MEDIUM): " + dateToStr);
+//
+//        dateToStr = DateFormat.getTimeInstance(DateFormat.LONG).format(currentDate);
+//        System.out.println("DateFormat.getTimeInstance(DateFormat.LONG): " + dateToStr);
+//
+//        dateToStr = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).format(currentDate);
+//        System.out.println("DateFormat.getTimeInstance(DateFormat.LONG): " + dateToStr);
+
+        Date date = new Date();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String strDate = formatter.format(date);
+        System.out.println("MM/dd/yyyy = " + strDate);
+
+        formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        strDate = formatter.format(date);
+        System.out.println("dd-M-yyyy hh:mm:ss = " + strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy");
+        strDate = formatter.format(date);
+        System.out.println("dd MMMM yyyy = " + strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy zzzz");
+        strDate = formatter.format(date);
+        System.out.println("dd MMMM yyyy zzzz = " + strDate);
+
+        formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+        strDate = formatter.format(date);
+        System.out.println("E, dd MMM yyyy HH:mm:ss z = " + strDate);
+
+        // Định dạng ngày tháng theo hai kiểu khác nhau
+        String YYYY_MM_DD = "yyyy-MM-dd";
+        String DD_MM_YYYY = "dd/MM/yyyy";
+
+        // Chuyển đổi LocalDate thành chuỗi
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern(YYYY_MM_DD);
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern(DD_MM_YYYY);
+
+        LocalDate localDate = LocalDate.now();
+
+        String dateFormatted1 = localDate.format(formatter1);
+        System.out.println("Formatted Date (yyyy-MM-dd): " + dateFormatted1);
+
+        String dateFormatted2 = localDate.format(formatter2);
+        System.out.println("Formatted Date (dd/MM/yyyy): " + dateFormatted2);
     }
 }
